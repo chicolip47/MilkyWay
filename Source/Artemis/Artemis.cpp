c75e4e93b7bff07804520bb37f4f72660001789e
chicolip47 <marcolip2001@gmail.com>
2026-06-11 13:05:40 +0200

    aggiustamenti per buiild

---
+++ Source/Artemis/Artemis.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Artemis.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Artemis, "Artemis" );
